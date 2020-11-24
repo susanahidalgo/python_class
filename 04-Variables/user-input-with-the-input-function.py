bf8183af4e63916c464842eb71ee7df955d630ed
Susana Hidalgo Fernández <sushidalgo95@gmail.com>
2020-11-24 18:23:14 +0100

    fixed user input

--- 04-Variables/user-input-with-the-input-function.py
+++ 04-Variables/user-input-with-the-input-function.py
@@ -1,6 +1,6 @@
 
 age= int(input("What's your age? "))
-favorite_number= int(input("What's your favorite number?")
+favorite_number= int(input("What's your favorite number?"))
 print ("so the final number is", age + favorite_number)
 
 

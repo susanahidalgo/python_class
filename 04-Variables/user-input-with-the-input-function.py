d6ef7baf0759db3a5a324369ccbf2e6065c39565
Susana Hidalgo Fernández <sushidalgo95@gmail.com>
2020-11-24 18:17:15 +0100

    user input with a function

--- 04-Variables/user-input-with-the-input-function.py
+++ 04-Variables/user-input-with-the-input-function.py
@@ -1,2 +1,6 @@
-first_name= input("What's your first name? ")
-print("It's nice to meet you", first_name)
+
+age= int(input("What's your age? "))
+favorite_number= int(input("What's your favorite number?")
+print ("so the final number is", age + favorite_number)
+
+

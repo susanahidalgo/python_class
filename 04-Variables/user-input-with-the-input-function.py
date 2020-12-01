3ee7713c2476507663bf3c622137638fa8593917
Susana Hidalgo Fernández <sushidalgo95@gmail.com>
2020-12-01 11:20:41 +0100

    exercises count and find

--- 04-Variables/user-input-with-the-input-function.py
+++ 04-Variables/user-input-with-the-input-function.py
@@ -4,3 +4,6 @@ favorite_number= int(input("What's your favorite number?"))
 print ("so the final number is", age + favorite_number)
 
 
+nationality=input("where you from? ")
+name=input("and what's your name? ")
+print("it is really nice to meet you", name, "from", nationality)
---
+++ 07-Strings-Methods/the-find-and-index-methods.py
@@ -0,0 +1,24 @@
+browser = "Google Chrome"
+
+print(browser.find("C"))
+
+print(browser.find("Ch"))
+
+print(browser.find("o"))
+
+print(browser.find("c"))
+
+print(browser.find("f"))
+
+print()
+
+print(browser.find("o"))
+print(browser.find("o", 2))
+print(browser.find("o", 5))
+
+print(browser.index("C"))
+
+random = "supercaligrafi"
+print(random.rfind("a"))
+print(random.rfind("i"))
+print(random.rfind("x"))
